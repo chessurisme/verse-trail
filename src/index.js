eb64d4ef2cff6c466380191e15830514b5035bf8
Kyle Angelo Galendez <chessurisme@gmail.com>
2025-11-27 12:40:56 +0800

    test: set-up scaffold

--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
 import setUpScaffold from './utilities/idb-set-up/set-up-scaffold'
-import saveReadChapter from './utilities/chapter/save-read'
-import getBookData from './utilities/chapter/get-book-data'
 
-window.addEventListener('DOMContentLoaded', () => {
-        setUpScaffold()
-        saveReadChapter('1', 1)
-        // saveReadChapter('1', 2)
-        // saveReadChapter('1', 3)
-
-        // const bookData = getBookData('1')
-        // console.log('Book Data of book 1: ', bookData)
+window.addEventListener('DOMContentLoaded', async () => {
+        await setUpScaffold()
 })
